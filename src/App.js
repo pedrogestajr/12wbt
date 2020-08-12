f037f8a6ba61bb0db3e001ce7aea3f4874a100e5
Pedro Gesta <pedrogestajr@gmail.com>
2020-08-13 00:55:14 +1000

    Import Hero component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+
 import NavMenu from "./components/NavMenu";
+import Hero from "./components/Hero";
 
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <Router>
       <NavMenu />
+      <Hero />
     </Router>
   );
 }
