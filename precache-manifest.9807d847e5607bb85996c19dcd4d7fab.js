435c7934de9a306e0c30a9fb61820a7d1a0678a6
Pedro Gesta <pedrogestajr@gmail.com>
2020-08-14 01:39:16 +1000

    Updates

---
+++ precache-manifest.9807d847e5607bb85996c19dcd4d7fab.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672a069497a22621dd2ae078179d520e",
+    "url": "/12wbt/index.html"
+  },
+  {
+    "revision": "eccd9dce4b1b284a1da2",
+    "url": "/12wbt/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "74f8699f08d14d96211c",
+    "url": "/12wbt/static/css/main.740a374d.chunk.css"
+  },
+  {
+    "revision": "eccd9dce4b1b284a1da2",
+    "url": "/12wbt/static/js/2.afa0c12e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/12wbt/static/js/2.afa0c12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f8699f08d14d96211c",
+    "url": "/12wbt/static/js/main.a3f63c73.chunk.js"
+  },
+  {
+    "revision": "2fce42cfe553fcce38d7",
+    "url": "/12wbt/static/js/runtime-main.bf7f7356.js"
+  },
+  {
+    "revision": "30c8c45ab0a610f77146b9f1e67fa0a8",
+    "url": "/12wbt/static/media/12wbt-logo.30c8c45a.30c8c45a.svg"
+  },
+  {
+    "revision": "685fed1fcf9e65f12d9d6074f15f6d56",
+    "url": "/12wbt/static/media/instagram-icon.685fed1f.png"
+  },
+  {
+    "revision": "1c2c3efff950294c3d2ae5a31125a324",
+    "url": "/12wbt/static/media/mishandtodd-hp.1c2c3eff.jpg"
+  },
+  {
+    "revision": "2046fee844ef6edef87cd951b8323180",
+    "url": "/12wbt/static/media/new-why-12wbt-will-work.2046fee8.jpg"
+  },
+  {
+    "revision": "343e820b6353991e6197641c9b7b2e6a",
+    "url": "/12wbt/static/media/newhp-header.343e820b.jpg"
+  },
+  {
+    "revision": "cb23eefcd5f6cea13c35a641026ea3b5",
+    "url": "/12wbt/static/media/post-baby.cb23eefc.jpg"
+  },
+  {
+    "revision": "2943f701f55076f5cda5a75d4cedbf75",
+    "url": "/12wbt/static/media/running.2943f701.jpg"
+  },
+  {
+    "revision": "765cf03db8363c838590d3d2277d6d1a",
+    "url": "/12wbt/static/media/strength.765cf03d.jpg"
+  },
+  {
+    "revision": "506770a678e00e105991eae21069e431",
+    "url": "/12wbt/static/media/weight-loss.506770a6.jpg"
+  }
+]);
