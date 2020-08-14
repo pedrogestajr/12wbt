e8449ebb880003058ddf62807712e89a6fae75d2
Pedro Gesta <pedrogestajr@gmail.com>
2020-08-14 19:06:26 +1000

    Import Results component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import NavMenu from "./components/NavMenu";
 import Hero from "./components/Hero";
 import Blokes from "./components/Blokes";
+import Results from "./components/Results";
 import WhyItWorks from "./components/WhyItWorks";
 import FitnessPrograms from "./components/FitnessPrograms";
 import Benefits from "./components/Benefits";
@@ -19,6 +20,7 @@ function App() {
       <NavMenu />
       <Hero />
       <Blokes />
+      <Results />
       <WhyItWorks />
       <FitnessPrograms />
       <Benefits />
